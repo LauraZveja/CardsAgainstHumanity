88ed5d07541892f279269986c982abec680dd358
Laura <lauramaisele@hotmail.lv>
2023-01-10 10:11:37 +0200

    added variables to user class

---
+++ src/main/java/model/User.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.Calendar;
+
+public class User {
+	
+	private String password;
+	private String userName;
+	private Calendar dateOfBirth; 
+	private String email;
+	private Calendar registrationDate;
+	private Gender gender;
+	private Country country;
+
+}
