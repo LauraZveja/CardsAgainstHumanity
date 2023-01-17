87b797b3dd4485361eea3f3063563d1cfbdd2e04
Laura <lauramaisele@hotmail.lv>
2023-01-17 16:09:54 +0200

    Added text file with answers.

--- src/main/java/model/AnswerCard.java
+++ src/main/java/model/AnswerCard.java
@@ -1,5 +1,36 @@
 package model;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 
 public class AnswerCard {
+	
+private ArrayList<String> adult_answers = new ArrayList<>();
+private ArrayList<String> underage_answers = new ArrayList<>();
+
+
+// man šķiet, ka setterus nevajag. Jāpajautā Karinai. 
+public ArrayList<String> getAdult_answers() {
+	return adult_answers;
+}
+
+public ArrayList<String> getUnderage_answers() {
+	return underage_answers;
+}
+
+public void readAdultTxt() {
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/ADULT.txt")))) {
+        String line;
+        while ((line = br.readLine()) != null) {
+            adult_answers.add(line);
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
 
 }
--- src/main/java/service/MainService.java
+++ src/main/java/service/MainService.java
@@ -1,8 +1,19 @@
 package service;
 
+import model.AnswerCard;
+
 public class MainService {
 
 	public static void main(String[] args) {
+		
+		AnswerCard ac = new AnswerCard();
+	    ac.readAdultTxt(); // call the method to read Adult.txt into adult_answers
+
+	    // test if it worked by printing out the contents of adult_answers
+	    for (String line : ac.getAdult_answers()) {
+	        System.out.println(line);
+	    }
+		
 		// TODO Auto-generated method stub
 
 	}
