afe3af1c5ac7e4e8a393f90455f1fbdc197b639e
Edgars Bērziņš <berzins.edgars.1992@gmail.com>
2023-01-25 22:24:32 +0200

    frame 3 scene

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+	@FXML
+	private TextField usernameField;
+
+	@FXML
+	private PasswordField passwordField;
+
+	@FXML
+	private Button buttonSavePlayer;
+
+	@FXML
+	public void saveButtonClick() {
+
+	}
+}
