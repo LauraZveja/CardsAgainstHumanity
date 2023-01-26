f4216c9c7b3cfd278b93081bd8866386740450f9
Laura <lauramaisele@hotmail.lv>
2023-01-26 14:45:40 +0200

    exitToMainPoga aizved atpakaļ uz main screen

--- src/main/java/controller/RegistrationController.java
+++ src/main/java/controller/RegistrationController.java
@@ -1,5 +1,36 @@
 package controller;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class RegistrationController {
+	
+	@FXML
+	private Button exitToMain;
+	
+	
+	public void backToMainScreen() {
+		
+		try {
+
+			Scene registrationScene = exitToMain.getScene();
+			Stage primaryStage = (Stage) registrationScene.getWindow();
+
+			Scene mainScene = FXMLLoader.load(getClass().getResource("/frame1_main.fxml"));
+
+			primaryStage.setScene(mainScene);
+			primaryStage.show();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 }
