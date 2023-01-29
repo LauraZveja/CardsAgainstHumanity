fac666cff90289e70e5f46a7402c00981e17f6fe
Edgars Bērziņš <berzins.edgars.1992@gmail.com>
2023-01-29 20:32:34 +0200

    RoundResults

--- src/main/java/controller/RoundResultController.java
+++ src/main/java/controller/RoundResultController.java
@@ -1,15 +1,193 @@
 package controller;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 import model.AnswerCard;
+import model.DatabaseUtils;
 import model.QuestionCard;
+import model.Round;
+import service.MainService;
 
 public class RoundResultController {
 
+	@FXML
+	private Label ResultsUsername1;
+
+	@FXML
+	private Label ResultsUsername2;
+
+	@FXML
+	private Label ResultsUsername3;
+
+	@FXML
+	private Label ResultsUsername1_1;
+
+	@FXML
+	private Label ResultsUsername1_2;
+
+	@FXML
+	private Label ResultsUsername1_3;
+
+	@FXML
+	private ImageView ResultsComputerCard1;
+
+	@FXML
+	private ImageView ResultsComputerCard2;
+
+	@FXML
+	private ImageView ResultsComputerCard3;
+
+	@FXML
+	private ImageView ResultsComputerCard4;
+
+	@FXML
+	private ImageView ResultsComputerCard5;
+
+	@FXML
+	private ImageView ResultsComputerCard6;
+
+	@FXML
+	private ImageView ResultsComputerCard7;
+
+	@FXML
+	private ImageView ResultsComputerCard8;
+
+	@FXML
+	private ImageView ResultsComputerCard9;
+
+	@FXML
+	private ImageView ResultsComputerCard10;
+
+	@FXML
+	private ImageView ResultsComputerCard11;
+
+	@FXML
+	private ImageView ResultsComputerCard12;
+
+	@FXML
+	private ImageView ResultsComputerCard13;
+
+	@FXML
+	private ImageView ResultsComputerCard14;
+
+	@FXML
+	private ImageView ResultsComputerCard15;
+
+	@FXML
+	private Label ResultsUsernamePlayer;
+
+	@FXML
+	private Button ReturnToLobbyFromResults;
+
+	@FXML
+	private Label ResultsGameID;
+
+	private int currentResultsGameID;
+
+	// GET GAME ID--------------------------
+	public void getResultsGameID() {
+		currentResultsGameID = DatabaseUtils.getLastLobbyID();
+	}
+
+	@FXML
+	private Label ResultsRoundID;
+
+	private int currentResultsRoundID;
+
+	public static Round round = new Round();
+
+	// GET ROUND ID--------------------------
+	public void getReslutsRoundId() {
+		currentResultsRoundID = round.getRoundID();
+	}
+
+	// RETURN TO LOBBY-------------------------------
+	@FXML
+	public void clickReturnToLobbyFromResults() {
+		try {
+			Scene scene_old = ReturnToLobbyFromResults.getScene();
+			Stage stage_primary = (Stage) scene_old.getWindow();
+			Scene scene_new = FXMLLoader.load(getClass().getResource("/frame5_lobby.fxml"));
+			stage_primary.setScene(scene_new);
+			stage_primary.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	public void initialize() {
+
+		// SHOW CURRENT PLAYER'S USERNAME----------
+		ResultsUsernamePlayer.setText(MainService.getCurrentPlayer().getUserName());
+
+		// SHOW GAME ID----------------------------
+		getResultsGameID();
+		ResultsGameID.setText(String.valueOf(currentResultsGameID));
+
+		// SHOW ROUND ID---------------------------
+		getReslutsRoundId();
+		ResultsRoundID.setText(String.valueOf(currentResultsRoundID));
+
+		// COMPUTER CARD BACK----------------------
+		ResultsComputerCard1.setImage(new Image("Card back.jpg"));
+		ResultsComputerCard2.setImage(new Image("Card back.jpg"));
+		ResultsComputerCard3.setImage(new Image("Card back.jpg"));
+		ResultsComputerCard4.setImage(new Image("Card back.jpg"));
+		ResultsComputerCard5.setImage(new Image("Card back.jpg"));
+		ResultsComputerCard6.setImage(new Image("Card back.jpg"));
+		ResultsComputerCard7.setImage(new Image("Card back.jpg"));
+		ResultsComputerCard8.setImage(new Image("Card back.jpg"));
+		ResultsComputerCard9.setImage(new Image("Card back.jpg"));
+		ResultsComputerCard10.setImage(new Image("Card back.jpg"));
+		ResultsComputerCard11.setImage(new Image("Card back.jpg"));
+		ResultsComputerCard12.setImage(new Image("Card back.jpg"));
+		ResultsComputerCard13.setImage(new Image("Card back.jpg"));
+		ResultsComputerCard14.setImage(new Image("Card back.jpg"));
+		ResultsComputerCard15.setImage(new Image("Card back.jpg"));
+	}
+
+	@FXML
+	private Button ContinueToNextRoundFromResults;
+
+	@FXML
+	public void clickContinueToNextRoundFromResults() {
+
+	}
+
+	@FXML
+	private Label ResultsQuestionCard;
+
+	@FXML
+	private Label ResultsAnswerCard1;
+
+	@FXML
+	private Label ResultsAnswerCard2;
+
+	@FXML
+	private Label ResultsAnswerCard3;
+
+	@FXML
+	private Label ResultsAnswerCardPlayer;
 
 	public void initData(Label questionText, AnswerCard radioAnswer, AnswerCard computerAnswer1,
 			AnswerCard computerAnswer2, AnswerCard computerAnswer3) {
-		// TODO Auto-generated method stub
-		
+
+		// GET COMPUTER ANSWERS FROM FRAME 7---------------------------------
+		ResultsAnswerCard1.setText(computerAnswer1.getAnswer());
+		ResultsAnswerCard2.setText(computerAnswer2.getAnswer());
+		ResultsAnswerCard3.setText(computerAnswer3.getAnswer());
+
+		// GET QUESTION CARD TEXT FROM FRAME 7-------------------------------
+		ResultsQuestionCard.setText(questionText.getText());
+
 	}
 }
