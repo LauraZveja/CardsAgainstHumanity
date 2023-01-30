88e4e15573766180d31efb1eb3c25b0a9ef589b2
Emīls Mangalis <emils.elemtiux@gmail.com>
2023-01-30 18:51:34 +0200

    insert admin account

--- src/main/resources/create_table.sql
+++ src/main/resources/create_table.sql
@@ -10,6 +10,8 @@ CREATE TABLE IF NOT EXISTS "Players" (
   "admin" INTEGER default 0 NOT NULL
 );
 
+INSERT INTO "Players" ("id", "username", "password", "dob", "email", "registerdate", "gender", "country", "admin") VALUES (0, "Admin", "esesmuadmins", NULL, "admin@cah.lv", NULL, "OTHER", "LATVIA", 1);
+
 CREATE TABLE IF NOT EXISTS "GameLobby" (
   "id" INTEGER PRIMARY KEY,
   "dateandtime" varchar(60),
